a2a2d27d237e4f5c67bfca91c85d249855a9de4a
henry <aengrhenry@gmail.com>
2022-05-22 08:31:37 -0700

    project initial setup and configurations

---
+++ schoolapp/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Student(models.Model):
+    first_name=models.CharField(max_length=100)
+    last_name=models.CharField(max_length=100)
+    admission_number=models.IntegerField(unique=True)
+    is_qualified=models.BooleanField(default=False)
+    grade=models.IntegerField(null=True, blank=True)
+    first_name=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.first_name
+
+    def get_grade(self):
+        if self.grade < 40:
+            return "Fail"
+        elif self.grade > 40 < 70:
+            return "Passed"
+        elif 70 < self.grade < 100:
+            return "Excellent"
+        else:
+            return "Repeat"
+
+
+class ClassRoom(models.Model):
+    name=models.CharField(max_length=200)
+    student_capacity=models.IntegerField()
+    students=models.ManyToManyField("Student")
+
+
+    def __str__(self):
+        return self.name
+            
